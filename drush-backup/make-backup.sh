b8c7ee87a75ba9ff04b3e8562134822da4c62bcf
b9sk <b9sk@users.noreply.github.com>
2018-10-08 19:57:28 +0300

    bash script syntax fix

--- drush-backup/make-backup.sh
+++ drush-backup/make-backup.sh
@@ -5,7 +5,7 @@ drush --root=/var/www/html cc all
 drush --root=/var/www/html vset maintenance_mode 1
 
 DRUSH_FILENAME=$(date +"%Y-%m-%d_%H%M")
-drush --root=/var/www/html ard --destination=/root/drush-backup/$DRUSH_FILENAME.tar --description=$1
+drush --root=/var/www/html ard --destination=/root/drush-backup/$DRUSH_FILENAME.tar --description="${DESC}"
 drush --root=/var/www/html vset maintenance_mode 0
 
 printf "\nJob done\n========\n"

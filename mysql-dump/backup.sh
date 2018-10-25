2a6b3ef8b3aef117cd93f20a3ef99feaf45dd1e0
b9sk <b9sk@users.noreply.github.com>
2018-10-25 12:50:45 +0300

    restoring DB properly

--- mysql-dump/backup.sh
+++ mysql-dump/backup.sh
@@ -7,5 +7,5 @@ if [ -z "${USER}" ] || [ -z "${PASSWD}" ] || [ -z "${DB}" ]; then
     echo -e "\nUsername, Password and Database name must not be empty!"
     echo -e "try: bash $0 db_username username_password db_name\n"
 else
-    mysqldump -h mysql -u${USER} -p${PASSWD} $DB > /root/mysql-dump/$DB-$(date +%F).sql
+    mysqldump -h mysql -u${USER} -p${PASSWD} $DB > /root/mysql-dump/$DB-$(date +%F_%H%M).sql
 fi
--- mysql-dump/restore.sh
+++ mysql-dump/restore.sh
@@ -8,5 +8,10 @@ if [ -z "${USER}" ] || [ -z "${PASSWD}" ] || [ -z "${DB}" ] || [ -z "${FILE_PATH
     echo -e "\nUsername, Password Database name and path/to/file.sql must not be empty!"
     echo -e "try: bash $0 db_username username_password db_name ./my_backup.sql\n"
 else
+	echo "Cleaning current database before restoring..."
+	mysql -h mysql -u${USER} -p${PASSWD} -Nse 'show tables' ${DB} | while read table; do mysql -h mysql -u${USER} -p${PASSWD} -e "drop table $table" ${DB}; done
+	echo "Restoring database ${DB}..."
     mysql -h mysql -u${USER} -p${PASSWD} $DB < $FILE_PATH
 fi
+
+

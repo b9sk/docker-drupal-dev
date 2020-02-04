04b7064fbbd4c9335678035929979a623c8b1a68
b9sk <b9sk@users.noreply.github.com>
2020-02-04 12:03:30 +0300

    docker compose version updated

--- ubuntu18.04-docker-post-install.sh
+++ ubuntu18.04-docker-post-install.sh
@@ -19,7 +19,7 @@ sudo apt-get update;
 
 sudo apt-get install -yqqq docker-ce docker-ce-cli containerd.io;
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
 sudo chmod +x /usr/local/bin/docker-compose;
 
 
